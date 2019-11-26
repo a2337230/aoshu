e05c52d8e66df70869dc865358f9375fb9c37f29
姜润丰 <qaz_xinlei@163.com>
2019-11-27 01:11:01 +0800

    1212

--- config/index.js
+++ config/index.js
@@ -35,6 +35,11 @@ module.exports = {
 				target: 'https://www2.xlxt.net',
 				secure: false,
 				changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+      },
+      '/Member/*': {
+				target: 'https://www2.xlxt.net',
+				secure: false,
+				changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
       }
     },
 
